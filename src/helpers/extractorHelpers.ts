750ccf0489bd86785fcf04c8ddc77473b03a7416
memanot <menemty@gmail.com>
2021-09-20 09:24:11 +0300

    .

--- src/helpers/extractorHelpers.ts
+++ src/helpers/extractorHelpers.ts
@@ -2,9 +2,10 @@ import { OnResponseReceivedEndpoint } from '../types/comments'
 import { SingleColumnWatchNextResults } from '../types/video-m'
 
 export function getCommentsRenderer(onResponseReceivedEndpoints?: OnResponseReceivedEndpoint[]) {
+  const slotId = 'RELOAD_CONTINUATION_SLOT_BODY'
   return (
     onResponseReceivedEndpoints
-      ?.find((x) => x.reloadContinuationItemsCommand?.slot === 'RELOAD_CONTINUATION_SLOT_BODY')
+      ?.find((x) => x.reloadContinuationItemsCommand?.slot === slotId)
       ?.reloadContinuationItemsCommand?.continuationItems?.filter(
         (x) => x.commentThreadRenderer?.comment?.commentRenderer
       )
@@ -13,9 +14,10 @@ export function getCommentsRenderer(onResponseReceivedEndpoints?: OnResponseRece
 }
 
 export function getRelatedItems(singleColumnWatchNextResults?: SingleColumnWatchNextResults) {
+  const sectionId = 'related-items'
   return (
     singleColumnWatchNextResults?.results?.results?.contents
-      ?.find((x) => x.itemSectionRenderer?.sectionIdentifier === 'related-items')
+      ?.find((x) => x.itemSectionRenderer?.sectionIdentifier === sectionId)
       ?.itemSectionRenderer?.contents?.filter((x) => x.videoWithContextRenderer)
       ?.map((x) => x.videoWithContextRenderer!) || []
   )
