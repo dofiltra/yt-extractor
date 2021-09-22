659916517fd24ec0b1ce4921808ec135120d5472
memanot <menemty@gmail.com>
2021-09-22 14:09:51 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { YtExtractorResponse } from './services/ytExtractorResponse'
 export { YtExtractor, YtExtractorResponse }
 
 /*
-https://www.youtube.com/feeds/videos.xml?channel_id=CHANNELID
+https://www.youtube.com/feeds/videos.xml?channel_id=UCY0xL8V6NzzFcwzHCgB8orQ
 https://www.youtube.com/feeds/videos.xml?user=USERNAME
 https://www.youtube.com/feeds/videos.xml?playlist_id=YOUR_YOUTUBE_PLAYLIST_NUMBER
 
